b890b197e0abe49e03dc1a0ee80ad83fabac875e
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-04-12 21:21:25 +0700

    Update entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,30 +1,26 @@
 # /bin/sh
 
-# Start PHP-FPM
-service php8.2-fpm start 
-
-
 #RUN component dev
 composer install && /
 php artisan blueprint:build && /
-rm -rf /var/www/html/vendor
+rm -rf /var/www/html/vendor && /
 
 # Run component deploy
-composer install --no-dev --optimize-autoloader
-php artisan migrate --seed --force
+composer install --no-dev --optimize-autoloader && /
+php artisan migrate --seed --force && /
 
 # Remove cache
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
+php artisan config:cache && /
+php artisan event:cache && /
+php artisan route:cache && /
+php artisan view:cache && /
 
-# Final configuration
-php artisan storage:link 
-php artisan key:generate
+# Final configuration 
+php artisan storage:link && / 
+php artisan key:generate && /
 
 # run Frankenphp
-exec frankenphp --config=/etc/frankenphp.json
+exec frankenphp --config=/etc/frankenphp.json && /
 
 # Keep container live
 tail -f /dev/null
