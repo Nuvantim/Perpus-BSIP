f78c888139c55cd135fbd5cd0fa7d2220dae6eab
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-04-13 13:37:16 +0700

    Update entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-# run Frankenphp in background
-frankenphp --config=/etc/frankenphp.json
+set -e
+
+# Check if app needs installation (you can use a marker file)
+if [ ! -f /var/www/html/.installed ]; then
+    echo "Running first-time installation..."
+    sh /var/www/html/install.sh
+    touch /var/www/html/.installed
+fi
+
+# Start FrankenPHP
+exec frankenphp run --config /etc/frankenphp.json
