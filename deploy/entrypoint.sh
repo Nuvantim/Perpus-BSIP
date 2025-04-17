5cc528722f59a3aa14ee94b5bd623f6ac82f05e2
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-04-17 15:32:35 +0700

    Update entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 set -e
 
+# Configure PHP-FPM to run in non-daemon mode
+sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php82/php-fpm.conf
+
 # Memeriksa apakah aplikasi perlu diinstal (menggunakan file penanda)
 if [ ! -f /var/www/html/.installed ]; then
     echo "Menjalankan instalasi pertama kali..."
-    touch /app/.installed
-    sh /app/install.sh
+    touch /var/www/html/.installed
+    sh /var/www/html/install.sh
 fi
+
+# Start PHP-FPM in background
+php-fpm82 &
+
+# Start Nginx in foreground
+nginx -g "daemon off;"
